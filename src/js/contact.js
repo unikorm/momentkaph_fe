d08b4e0c4a4e190caee0784530de32ce99a356a9
adamko <adaled00@gmail.com>
2026-09-13 00:31:02 +0200

    detail

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,7 +1,7 @@
 /** Contact form. The browser does the validating; we just send it. */
 const form = document.querySelector('form');
 const button = form.querySelector('button');
-const API = 'https://api.momentkaph.sk';
+const API = location.hostname === 'localhost' ? 'http://localhost:3069' : 'https://api.momentkaph.sk';
 
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
